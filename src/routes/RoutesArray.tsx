a7a1f1ff59c4bf1158dcde4fe7c68f93411d5947
Martin Johnsson <maartinjohnsson@gmail.com>
2024-05-20 14:00:48 +0200

    Add Layout component and wraps it around all routes

--- src/routes/RoutesArray.tsx
+++ src/routes/RoutesArray.tsx
@@ -1,21 +1,25 @@
 import { RouteObject, Navigate } from 'react-router-dom';
 import Users from './pages/users/Users';
 import NewPlace from './pages/places/NewPlace';
+import BaseLayout from '../components/shared/Layout/Layout';
 
 export const routes: RouteObject[] = [
   {
-    path: '/',
-    index: true,
-    element: <Users />,
-  },
-  {
-    path: '/places/new',
-    index: true,
-    element: <NewPlace />,
-  },
-
-  {
-    path: '*',
-    element: <Navigate to='/' replace />,
+    element: <BaseLayout />,
+    children: [
+      {
+        path: '/',
+        index: true,
+        element: <Users />,
+      },
+      {
+        path: '/places/new',
+        element: <NewPlace />,
+      },
+      {
+        path: '*',
+        element: <Navigate to='/' replace />,
+      },
+    ],
   },
 ];
