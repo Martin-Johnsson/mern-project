b576aed934a2cd7a51713fe4ebdbfb664186f4df
Martin Johnsson <maartinjohnsson@gmail.com>
2024-05-28 22:04:46 +0200

    Add ternary to use different routes depending on auth in RoutesArray

--- src/routes/RoutesArray.tsx
+++ src/routes/RoutesArray.tsx
@@ -5,8 +5,9 @@ import NewPlace from './pages/places/NewPlace/NewPlace';
 import BaseLayout from '../shared/Layout/Layout';
 import UserPlaces from './pages/places/UserPlaces/UserPlaces';
 import UpdatePlace from './pages/places/UpdatePlace/UpdatePlace';
+import Auth from './pages/auth/Auth';
 
-export const routes: RouteObject[] = [
+export const AuthenticatedRoutes: RouteObject[] = [
   {
     element: <BaseLayout />,
     children: [
@@ -29,6 +30,30 @@ export const routes: RouteObject[] = [
       {
         path: '*',
         element: <Navigate to='/' replace />,
+      },
+    ],
+  },
+];
+
+export const UnAuthenticatedRoutes: RouteObject[] = [
+  {
+    element: <BaseLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Users />,
+      },
+      {
+        path: ':userId/places',
+        element: <UserPlaces />,
+      },
+      {
+        path: 'auth',
+        element: <Auth />,
+      },
+      {
+        path: '*',
+        element: <Navigate to='/auth' replace />,
       },
     ],
   },
