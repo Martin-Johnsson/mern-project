bf07234f994501f29d3ecc2d1d56f1c926f813ec
Martin Johnsson <maartinjohnsson@gmail.com>
2024-05-30 21:48:11 +0200

    Add users-controllers.js and controllers for getUsers, createUser and loginUser

---
+++ backend/controllers/users-controllers.js
@@ -0,0 +1,62 @@
+const { v4: uuidv4 } = require('uuid');
+
+const HttpError = require('../models/http-error');
+
+const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Martin Johnsson',
+    email: 'test@live.se',
+    password: 'supersecretpassword',
+    image: 'https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg',
+    places: 3,
+  },
+];
+
+const getUsers = (req, res, next) => {
+  res.status(200).json({ users: DUMMY_USERS });
+};
+
+const createUser = (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const hasUser = DUMMY_USERS.find((user) => user.email === email);
+
+  if (hasUser) {
+    throw new HttpError('Could not create user, email already exists.', 422);
+  }
+
+  const createdUser = {
+    id: uuidv4(),
+    name,
+    email,
+    password,
+    image: '',
+    places: 0,
+  };
+
+  DUMMY_USERS.push(createdUser);
+
+  res.status(201).json({ user: createdUser });
+};
+
+const loginUser = (req, res, next) => {
+  const { email, password } = req.body;
+
+  const identifiedUser = DUMMY_USERS.find(
+    (user) => user.email === email && user.password === password
+  );
+
+  if (!identifiedUser) {
+    throw new Error(
+      'Could not identiy user, credentials seem to be wrong.',
+      401
+    );
+  }
+
+  res.status(200).json({ User: identifiedUser });
+};
+
+exports.getUsers = getUsers;
+exports.createUser = createUser;
+exports.loginUser = loginUser;
