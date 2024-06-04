e04d8a043168ac15c94b7a558367e7e4e8aad18e
Martin Johnsson <maartinjohnsson@gmail.com>
2024-06-04 20:58:06 +0200

    Add function for connecting to Google API generating coordinates from address

--- backend/routes/placesRoutes.js
+++ backend/routes/placesRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { check } = require('express-validator');
 
 const placesControllers = require('../controllers/places-controllers');
 
@@ -7,9 +8,21 @@ router.get('/:placeId', placesControllers.getPlaceById);
 
 router.get('/user/:userId', placesControllers.getPlacesByUserId);
 
-router.post('/', placesControllers.createPlace);
+router.post(
+  '/',
+  [
+    check('title').not().isEmpty(),
+    check('description').isLength({ min: 5 }),
+    check('address').not().isEmpty(),
+  ],
+  placesControllers.createPlace
+);
 
-router.patch('/:placeId', placesControllers.updatePlaceById);
+router.patch(
+  '/:placeId',
+  [check('title').not().isEmpty(), check('description').isLength({ min: 5 })],
+  placesControllers.updatePlaceById
+);
 
 router.delete('/:placeId', placesControllers.deletePlaceById);
 
