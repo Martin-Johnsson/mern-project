465aad2a6fb90df797a1bcd1534f3e2b7a9ba93f
Martin Johnsson <maartinjohnsson@gmail.com>
2024-06-13 22:08:10 +0200

    Add deletion of saved img upon errors

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,30 @@ const dotenv = require('dotenv');
 const placesRoutes = require('./routes/placesRoutes');
 const usersRoutes = require('./routes/usersRoutes');
 const HttpError = require('./models/http-error');
+const fs = require('fs');
+
+const path = require('path');
 
 dotenv.config();
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type',
+    'Accept',
+    'Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+  next();
+});
 
 app.use('/api/places', placesRoutes);
 
@@ -23,6 +41,11 @@ app.use((req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
+  if (req.file) {
+    fs.unlink(req.file.path, (err) => {
+      console.log(err);
+    });
+  }
   if (res.headerSent) {
     return next(error);
   }
