fce22e6d1f6b9b70d3d67418c9e18de368c232e9
Martin Johnsson <maartinjohnsson@gmail.com>
2024-06-30 19:43:46 +0200

    UPDATE check-auth to utilize TypeScript

--- backend/middleware/check-auth.ts
+++ backend/middleware/check-auth.ts
@@ -1,22 +1,29 @@
-const jwt = require('jsonwebtoken');
+/// <reference path="../types/.d.ts" />
+
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 const HttpError = require('../models/http-error');
 
-module.exports = (req, res, next) => {
+module.exports = (req: Request, res: Response, next: NextFunction) => {
   if (req.method === 'OPTIONS') {
     return next();
   }
   try {
-    const token = req.headers.authorization.split(' ')[1];
+    const token: string | undefined = req.headers.authorization?.split(' ')[1];
 
     if (!token) {
       throw new Error('Authentication failed!');
     }
     const decodedToken = jwt.verify(token, 'supersecret_dont_share');
 
+    if (typeof decodedToken === 'object' && 'userId' in decodedToken) {
     req.userData = { userId: decodedToken.userId };
     next();
-  } catch (err) {
+  } } catch (err) {
     const error = new HttpError('Authentication failed', 403);
     return next(error);
-  }
-};
+      }
+}
+
+
+
