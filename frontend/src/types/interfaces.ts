5f60aeb5d934e71f12f02dd6dc1a90b547c18e7a
Martin Johnsson <maartinjohnsson@gmail.com>
2024-07-09 22:35:47 +0200

    Add types for Input component

--- frontend/src/types/interfaces.ts
+++ frontend/src/types/interfaces.ts
@@ -8,3 +8,23 @@ export interface IImageUploadProps {
     fileIsValid: boolean
   ) => void;
 }
+
+export interface IInputState {
+  value: string;
+  isValid: boolean;
+  isTouched: boolean;
+}
+
+export interface IInput {
+  initialValue: string;
+  initialValid: boolean;
+  id: string;
+  onInput: (id: string, value: string, isValid: boolean) => void;
+  validators: [];
+  element: string;
+  type: string;
+  placeholder: string;
+  rows: number;
+  label: string;
+  errorText: string;
+}
