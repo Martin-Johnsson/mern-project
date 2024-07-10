0d5c17898fc5e4714ddfd18a577c00db5f13e797
Martin Johnsson <maartinjohnsson@gmail.com>
2024-07-10 22:29:27 +0200

    ADD types for Cardp

--- frontend/src/shared/components/UIElements/Card/Card.tsx
+++ frontend/src/shared/components/UIElements/Card/Card.tsx
@@ -1,6 +1,7 @@
+import { ICardProps } from '../../../../types/Interfaces';
 import './Card.css';
 
-const Card = (props) => {
+const Card: React.FC<ICardProps> = (props) => {
   return (
     <div className={`card ${props.className}`} style={props.style}>
       {props.children}
