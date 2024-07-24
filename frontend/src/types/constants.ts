0ccf998c3ef066ba6987545e91568988dc3e8c81
Martin Johnsson <maartinjohnsson@gmail.com>
2024-07-24 22:14:07 +0200

    ADD: types for constants.ts

--- frontend/src/types/constants.ts
+++ frontend/src/types/constants.ts
@@ -1,7 +1,7 @@
-export const VALIDATOR_TYPE_REQUIRE = 'REQUIRE';
-export const VALIDATOR_TYPE_MINLENGTH = 'MINLENGTH';
-export const VALIDATOR_TYPE_MAXLENGTH = 'MAXLENGTH';
-export const VALIDATOR_TYPE_MIN = 'MIN';
-export const VALIDATOR_TYPE_MAX = 'MAX';
-export const VALIDATOR_TYPE_EMAIL = 'EMAIL';
-export const VALIDATOR_TYPE_FILE = 'FILE';
+export const VALIDATOR_TYPE_REQUIRE: string = 'REQUIRE';
+export const VALIDATOR_TYPE_MINLENGTH: string = 'MINLENGTH';
+export const VALIDATOR_TYPE_MAXLENGTH: string = 'MAXLENGTH';
+export const VALIDATOR_TYPE_MIN: string = 'MIN';
+export const VALIDATOR_TYPE_MAX: string = 'MAX';
+export const VALIDATOR_TYPE_EMAIL: string = 'EMAIL';
+export const VALIDATOR_TYPE_FILE: string = 'FILE';
