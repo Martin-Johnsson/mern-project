097c6c15a1154fe3ba043c740d8e25f8d9389381
Martin Johnsson <maartinjohnsson@gmail.com>
2024-08-12 22:25:28 +0200

    UPDATE: check-auth to utilize environment variable

--- backend/middleware/check-auth.ts
+++ backend/middleware/check-auth.ts
@@ -14,16 +14,14 @@ module.exports = (req: Request, res: Response, next: NextFunction) => {
     if (!token) {
       throw new Error('Authentication failed!');
     }
-    const decodedToken = jwt.verify(token, 'supersecret_dont_share');
+    const decodedToken = jwt.verify(token, process.env.JWT_KEY as string);
 
     if (typeof decodedToken === 'object' && 'userId' in decodedToken) {
-    req.userData = { userId: decodedToken.userId };
-    next();
-  } } catch (err) {
+      req.userData = { userId: decodedToken.userId };
+      next();
+    }
+  } catch (err) {
     const error = new HttpError('Authentication failed', 403);
     return next(error);
-      }
-}
-
-
-
+  }
+};
